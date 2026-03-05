37f3bf436c99bc0c470db3c2446a35922a45629f
Rey-A-Vasquez <official.rv.7394@gmail.com>
2026-03-05 10:41:34 -0800

    Make dropdown functional and added JavaScript
    Added all careers to the drop down menu. Began basis for taking in input values and adding them up for the chart.

--- functionality.js
+++ functionality.js
@@ -22,6 +22,72 @@ function init() {
 
         });
     });
+
+    async function getCareers() {
+        const url = "https://eecu-data-server.vercel.app/data";
+        try {
+            const response = await fetch(url);
+            const jobs = await response.json();
+            createOptions(jobs);
+            return jobs;
+        }
+        catch (error) {
+            console.error("Error fetching careers data:", error);
+            return [];
+        }
+    }
+
+    function createOptions(careers) {
+        const dropdown = document.getElementById("career");
+
+        careers.forEach((career, index) => {
+        const option = document.createElement("option");
+
+         option.innerHTML = `${career.Occupation}: $${career.Salary}`;
+         option.setAttribute("id", `${index}`);
+
+        dropdown.appendChild(option);
+        });
+
+        dropdown.addEventListener("input", (input) => { //maybe replace with "options.addEventListener"?
+            console.log(input); //figure out a way to grab necessary information based off which option was clicked
+        })
+    }
+    getCareers();
+
+    function calcSaveChart() {
+        const inputs = document.getElementsByClassName("expense"); //grab all input boxes
+        const savedExpenses = {};
+        let house = 0;
+        let transport = 0; //variables for chart and totals
+        let education = 0;
+        let food = 0;
+        let savings = 0;
+
+        let total = 0;
+        inputs.forEach(input => {
+            total += Number(input.value.replace(/[^0-9]/g, '')) || 0; //adds input value to sum, only takes integers
+            savedExpenses[input.id] = Number(input.value.replace(/[^0-9]/g, '')) || 0; //adds input to object
+
+            if(input.classList.contains("house")) {
+                house += Number(input.value.replace(/[^0-9]/g, '')) || 0; //checks class list, if match, add to class total
+            }
+            else if(input.classList.contains("transport")) {
+                transport += Number(input.value.replace(/[^0-9]/g, '')) || 0;
+            }
+            else if(input.classList.contains("education")) {
+                education += Number(input.value.replace(/[^0-9]/g, '')) || 0;
+            }
+            else if(input.classList.contains("food")) {
+                food += Number(input.value.replace(/[^0-9]/g, '')) || 0;
+            }
+            else if(input.classList.contains("savings")) {
+                savings += Number(input.value.replace(/[^0-9]/g, '')) || 0;
+            }
+
+            localStorage.setItem("savedExpenses", JSON.stringify(savedExpenses)); //saving object
+        });
+    }
 }
 
 
